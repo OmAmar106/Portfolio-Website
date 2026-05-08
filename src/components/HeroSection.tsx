2352a1629347ae1fcbfecc761b861ed7aa57e50b
Om Amar <emailomamar@gmail.com>
2026-05-08 18:38:51 +0530

    updated:stats

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -9,7 +9,7 @@ const lines = [
   {
     prefix: "const",
     text: [
-      { value: ' intern = "Upcoming Summer Intern @ ' },
+      { value: ' intern = "Summer Intern @ ' },
       { value: "G", className: "text-[#4285F4]" },
       { value: "o", className: "text-[#EA4335]" },
       { value: "o", className: "text-[#FBBC05]" },
